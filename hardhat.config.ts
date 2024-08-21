b29711be540d34a1aaa817137f4e205d4bd34e6b
ujjwalareddy1 <ujjwala622@gmail.com>
2024-08-21 10:53:43 +0530

    Add files via upload

---
+++ hardhat.config.ts
@@ -0,0 +1,43 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+// https://github.com/projectsophon/hardhat-circom
+import "hardhat-circom";
+// circuits
+import circuits = require('./circuits.config.json')
+
+// set env var to the root of the project
+process.env.BASE_PATH = __dirname;
+
+// tasks
+import "./tasks/newcircuit.ts"
+import { network } from "hardhat";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+      },
+      {
+        version: "0.6.11",
+      }
+    ]
+  },
+  networks: {
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/7a450d4492094e3b991f70855552ddb9",
+      chainId: 11155111,
+      accounts: ["2383258e510adc86fa59f266948cc62a82f0335b70dff9a81dcea2f8b694a1fe"]
+    }
+  },
+  circom: {
+    // (optional) Base path for input files, defaults to `./circuits/`
+    inputBasePath: "./circuits",
+    // (required) The final ptau file, relative to inputBasePath, from a Phase 1 ceremony
+    ptau: "powersOfTau28_hez_final_12.ptau",
+    // (required) Each object in this array refers to a separate circuit
+    circuits: JSON.parse(JSON.stringify(circuits))
+  },
+};
+
+export default config;
