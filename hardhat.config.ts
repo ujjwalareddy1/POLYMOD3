1a29cdd6efa8f67dbf3ecc56026089b8cf391b98
ujjwalareddy1 <ujjwala622@gmail.com>
2024-08-28 10:42:16 +0530

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,13 +4,12 @@ import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-circom";
 // circuits
 import circuits = require('./circuits.config.json')
-
+import 'dotenv/config';
 // set env var to the root of the project
 process.env.BASE_PATH = __dirname;
 
 // tasks
 import "./tasks/newcircuit.ts"
-import { network } from "hardhat";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -23,15 +22,15 @@ const config: HardhatUserConfig = {
       }
     ]
   },
-  networks: {
+  networks:{
+    hardhat: {},
     sepolia: {
-      url: "https://sepolia.infura.io/v3/7a450d4492094e3b991f70855552ddb9",
-      chainId: 11155111,
-      accounts: ["2383258e510adc86fa59f266948cc62a82f0335b70dff9a81dcea2f8b694a1fe"]
+      url: 'https://rpc.sepolia.org',
+      accounts: [process.env.PRIVATE_KEY]
     }
   },
   circom: {
-    // (optional) Base path for input files, defaults to `./circuits/`
+    // (optional) Base path for input files, defaults to ./circuits/
     inputBasePath: "./circuits",
     // (required) The final ptau file, relative to inputBasePath, from a Phase 1 ceremony
     ptau: "powersOfTau28_hez_final_12.ptau",
